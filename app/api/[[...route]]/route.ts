115e1f872c061426b30db9ec8205f6888e8eb49b
prajun <prajunbudathoki1@gmail.com>
2025-11-07 13:41:25 +0545

    wip

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,11 +1,28 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+import test from "node:test";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
-  return c.json({ message: "Hello, World!" });
-});
+app
+  .get("/hello", (c) => {
+    return c.json({ message: "Hello, World!" });
+  })
+  .get(
+    "/hello/:test",
+    zValidator(
+      "param",
+      z.object({
+        test: z.number(),
+      })
+    ),
+    (c) => {
+      const test = c.req.param("test");
+      return c.json({ message: `Hello, ${test}!` });
+    }
+  );
 
 export const GET = handle(app);
 export const POST = handle(app);
